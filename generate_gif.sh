61bb21a3d72f3cb4dfbfeb5c2df029821ae48aec
Felix Z. Hoffmann <felix11h.dev@gmail.com>
2017-04-26 09:44:07 +0200

    add pause after loop for readability

--- generate_gif.sh
+++ generate_gif.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-convert -density 600 -resize 800x -delay 150 -loop 0 main.pdf main_animated.gif    
+convert -density 600 -resize 800x -delay 200 -loop 0 main.pdf main_animated.gif    
+convert main_animated.gif \( +clone -set delay 500 \) +swap +delete main_animated.gif
